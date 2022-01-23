989aa265334622f0fae9ef5d519bda6583c8edb9
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-01-23 15:39:41 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let PixelArray: neopixel.Strip = null
 /**
  * KITRONIK :MOVE NEEDED
  */
@@ -42,26 +41,25 @@ input.onGesture(Gesture.Shake, function () {
         pins.servoWritePin(AnalogPin.P2, 180)
     }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    PixelArray.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Red))
-    PixelArray.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    PixelArray.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
-    basic.showString(":MovePixels for Micro:bit V2 & Kitronik :MOVE mini Mk2")
-    basic.clearScreen()
-    basic.showString("Extension by AdaFruit")
-    for (let index = 0; index < 4; index++) {
-        PixelArray.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(1000)
-        PixelArray.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.pause(1000)
-        PixelArray.showColor(neopixel.colors(NeoPixelColors.Blue))
-        basic.pause(1000)
-    }
-    PixelArray.showRainbow(1, 330)
-})
+let PixelArray: neopixel.Strip = null
+PixelArray = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+PixelArray.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Red))
+PixelArray.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+PixelArray.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
+basic.showString(":MovePixels for Micro:bit V2 & Kitronik :MOVE mini Mk2")
+basic.clearScreen()
+basic.showString("Extension by AdaFruit")
+for (let index = 0; index < 4; index++) {
+    PixelArray.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    PixelArray.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(1000)
+    PixelArray.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(1000)
+}
+PixelArray.showRainbow(1, 330)
 basic.forever(function () {
     input.setSoundThreshold(SoundThreshold.Loud, 128)
-    PixelArray = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
     PixelArray.setBrightness(10)
     radio.setGroup(8.456)
 })
